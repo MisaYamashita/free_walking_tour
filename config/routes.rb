2adbeada3d4d4bd5886b056a79739860a2d3fe6c
misa <mi-426-sa@hotmail.co.jp>
2020-09-25 00:56:34 +0000

    signup routed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'users#new'
   root 'pages#home'
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
