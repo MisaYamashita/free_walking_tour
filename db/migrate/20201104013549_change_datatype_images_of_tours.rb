43dbf434194dba474518a9d0e4263e54f9f38892
misa <mi-426-sa@hotmail.co.jp>
2020-11-12 17:26:12 +0900

    heroku

--- db/migrate/20201104013549_change_datatype_images_of_tours.rb
+++ db/migrate/20201104013549_change_datatype_images_of_tours.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeImagesOfTours < ActiveRecord::Migration[5.2]
   def change
-    change_column :tours, :images, :json
+    change_column :tours, :images, :json , 'json USING CAST(images AS json)'
   end
 end
